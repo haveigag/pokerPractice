f069aaeb4cc6203942b1cfc98a1d788bfe320019
Henry Aveiga <haveigag@gmail.com>
2021-04-16 02:17:01 -0500

    Refactor the creation of cards

--- src/script.js
+++ src/script.js
@@ -1,43 +1,43 @@
-
 function createTopSuitTag( suit, rank ){
-  let topSuit = document.createElement("div");
-  topSuit.innerText = name;
-  topSuit.classList.add("suit-top");
-  if( name.includes("♥") ||  name.includes("♦") ){
-    topSuit.classList.add("red");
-  }
-  var card = document.getElementById(name);
-  card.appendChild(topSuit);
-}
+  let name = suit + rank;
+  const suitType = ['suit-top', 'suit-bottom'];
+  for( let i = 0; i < suitType.length; i++){
+    let container = document.createElement("div");
+    container.classList.add(suitType[i]);
 
-function createBottomSuitTag(name){
-  let bottomSuit = document.createElement("div");
-  bottomSuit.innerText = name;
-  bottomSuit.classList.add("suit-bottom");
-  if( name.includes("♥") ||  name.includes("♦") ){
-    bottomSuit.classList.add("red");
+    let suitDiv = document.createElement("div");
+    let rankDiv = document.createElement("div");
+    suitDiv.innerText = suit;
+    rankDiv.innerText = rank;
+
+    if( suit === "♥" ||  suit === "♦" ){
+      container.classList.add("red");
+    }
+    container.appendChild(suitDiv);
+    container.appendChild(rankDiv);
+
+    var card = document.getElementById(name);
+    card.appendChild(container);
   }
-  var card = document.getElementById(name);
-  card.appendChild(bottomSuit);
 }
 
 function createCard( suit, rank ) {
-  name =  suit + rank;
+  let name =  suit + rank;
   let card = document.createElement( "div" );
   card.classList.add("card");
   card.id = name;
   document.body.appendChild(card);
+  deck.push( card );
   createTopSuitTag( suit, rank );
-  createBottomSuitTag(name);
 }
 
 function createDeck(){
   let suits = [ "♠", "♥", "♣", "♦"];
   let ranks = [ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-  let deck = [];
+  deck = [];
   for( let s = 0; s < suits.length; s++ ){
 	  for( let i = 0; i < ranks.length; i++ ){
-      deck.push(suits[s] + ranks[i] );
+      //deck.push(suits[s] + ranks[i] );
       createCard( suits[s], ranks[i] );
 	  }
   }
